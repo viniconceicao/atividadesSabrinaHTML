fb95b0e22a08833e59c2a89b2720b20e387a631b
Vinicius de Liz da Conceição <130084519+viniconceicao@users.noreply.github.com>
2025-04-22 16:14:17 -0300

    Corrigido: removidos submódulos, adicionados arquivos reais

---
+++ 03/README.md
@@ -0,0 +1,4 @@
+# SabrinaCSSHTML
+Um git criado para entrega de atividade da Sabrina, utilizei HTML, CSS e JavaScript.
+
+Nome: Vinicius de Liz da Conceição
---
+++ 03/script.js
@@ -0,0 +1,5 @@
+function mostrarAlerta() {
+    alert('Parabéns! Você enviou o seu formulário, prazer em conhecê-lo(a)!');
+};
+
+// Eu ia usar métodos de bootstrap porém achei que fazer muita coisa além da atividade kkkkk
